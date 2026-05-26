91810ecb9a59278dcdd072f06e6be09084e20c8f
Faaz <mr.fas1012@gmail.com>
2026-05-26 21:40:41 +0400

    Scaffold multi-dashboard project structure and login routing

---
+++ Akshaya-Ak/config.js
@@ -0,0 +1,6 @@
+window.DASHBOARD_CONFIG = {
+  id: "Akshaya-Ak",
+  title: "Akshaya-Ak Dashboard",
+  theme: "default",
+  routes: ["home", "journal", "music", "extras"]
+};
---
+++ Faaz-Fz/config.js
@@ -0,0 +1,6 @@
+window.DASHBOARD_CONFIG = {
+  id: "Faaz-Fz",
+  title: "Faaz-Fz Dashboard",
+  theme: "default",
+  routes: ["home", "journal", "music", "extras"]
+};
